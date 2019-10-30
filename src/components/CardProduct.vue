b208f6451e14031a4932b1348194ece7cdf7f4dc
Elias Bernardo <ebmm01@gmail.com>
2019-10-30 17:23:43 -0300

    Adding products

---
+++ src/components/CardProduct.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="card-item">
+        <img :src="img" alt="">
+        <div>
+            <p class="mb-1">{{value}}<small class="risked"> {{nonvalue}}</small></p>
+            <p class="gray">1000+ compraram esse produto</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        img : {
+            type: String
+        },
+        value: {
+            type: String
+        },
+        nonvalue: {
+            type: String
+        },
+    }
+}
+</script>
+
+<style lang="scss">
+.card-item {
+    width: 100%;
+    height: 280px;
+    display: flex;
+    flex-direction: column;
+    background: white;
+    box-shadow: rgba(0, 0, 0, 0.05) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 2px 20px 0px;
+    transition: all 0.3s ease;
+
+    &:hover {
+        transform: scale(1.05);
+        z-index: 1;
+        overflow: visible;
+    }
+
+    img {
+        width: 100%;
+        height: 220px;
+        object-fit: cover;
+    }
+
+    p {
+        margin: 0 10px;
+        color: black;
+
+        &.gray {
+            color: rgb(126, 154, 166);
+            font-size: 12px;
+        }
+
+        small {
+            padding-left: 10px;
+            text-decoration: line-through;
+            color: rgb(126, 154, 166);
+        }
+    }
+
+
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,62 +1,50 @@
 <template>
-  <v-container>
-    ...
-  </v-container>
+    <v-container>
+        <v-row style="max-width: 1200px; margin: auto">
+            <template v-for="n in 10" >
+                <v-col cols="12" sm="6" md="4" lg="3" :key="n">
+                    <CardProduct 
+                        img="https://contestimg.wish.com/api/webimage/5d530f33538bd178f3ca4ed7-0-feed?cache_buster=f02fe347bf324e1f51a4156b8a50d3df&cozy=1"
+                        value="R$ 50,00"
+                        nonvalue="R$ 200,00"/>
+                </v-col>
+                <v-col cols="12" sm="6" md="4" lg="3" :key="n">
+                    <CardProduct 
+                        img="https://contestimg.wish.com/api/webimage/5d1b3b26136dca0a8f73ec28-0-feed?cache_buster=b9897f1238cd4b23afa9179872bbc87c&cozy=1"
+                        value="R$ 48,00"
+                        nonvalue="R$ 305,00"/>
+                </v-col>
+                <v-col cols="12" sm="6" md="4" lg="3" :key="n">
+                    <CardProduct 
+                        img="https://contestimg.wish.com/api/webimage/5d5a37de1e1d981d24fe02b0-0-feed?cache_buster=2a24fbacbc06b5f29572fd41b2403442&cozy=1"
+                        value="R$ 15,00"
+                        nonvalue="R$ 50,00"/>
+                </v-col>
+                <v-col cols="12" sm="6" md="4" lg="3" :key="n">
+                    <CardProduct 
+                        img="https://contestimg.wish.com/api/webimage/5d7b96ca9c40b0162a9dbac7-0-feed?cache_buster=fde871fa667a348b1bc184e75057ce7f&cozy=1"
+                        value="R$ 302,00"
+                        nonvalue="R$ 1.200,00"/>
+                </v-col>
+                <v-col cols="12" sm="6" md="4" lg="3" :key="n">
+                    <CardProduct 
+                        img="https://contestimg.wish.com/api/webimage/5d71a5109c80e72d17d04090-0-feed?cache_buster=b4351e890183c9c686e50051da7eac1a&cozy=1"
+                        value="R$ 6,00"
+                        nonvalue="R$ 37,00"/>
+                </v-col>
+            </template>
+        </v-row>
+        
+    </v-container>
 </template>
 
 <script>
+import CardProduct from './CardProduct'
+
 export default {
-  data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader',
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify',
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
-    ],
-    importantLinks: [
-      {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com',
-      },
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer',
-      },
-      {
-        text: 'Select a layout',
-        href: 'https://vuetifyjs.com/layout/pre-defined',
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
-    ],
-  }),
+    data: () => ({}),
+    components: {
+        CardProduct
+    }
 };
 </script>
