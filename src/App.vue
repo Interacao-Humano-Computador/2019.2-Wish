7b92f29d51636025dd21d2ad5dae8cb7517f9898
Elias Bernardo <ebmm01@gmail.com>
2019-10-30 16:44:47 -0300

    Adding prototip

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <NavbarWish/>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import NavbarWish from './components/NavbarWish'
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    NavbarWish
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Noto+Sans:400,700&display=swap');
+
+body * {
+    font-family: 'Noto Sans', sans-serif;
+}
+</style>
